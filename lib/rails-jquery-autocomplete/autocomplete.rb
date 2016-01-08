71abe4ccff9a26fd806f14419abd5a2dd91c2ac5
Daniel Prejean <daniel@etszone.com>
2016-01-08 16:51:12 -0600

    A little more code formatting

--- lib/rails-jquery-autocomplete/autocomplete.rb
+++ lib/rails-jquery-autocomplete/autocomplete.rb
@@ -72,8 +72,13 @@ module RailsJQueryAutocomplete
             object_method_hash.each do |object, method|
               # allow specifying fully qualified class name for model object
               # both object and method can be specified by object or id
-              items = get_autocomplete_items(object, :model => get_object(object), \
-                :options => options, :term => term, :method => method)
+              items = get_autocomplete_items(
+                  object,
+                  :model   => get_object(object),
+                  :options => options,
+                  :term    => term,
+                  :method  => method
+              )
               json += json_for_autocomplete(items, \
                 options[:display_value] ||= method, options[:extra_data], &block)
             end
